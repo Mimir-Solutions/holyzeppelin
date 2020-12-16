38d46ed481eeaaea9c980681b294b78137f39253
cyotee doge <cyotee@gmail.com>
2020-12-16 04:35:40 -0800

    Moved IERC721 to interfaces directory
    Moved EnumerbableSet to collections
    
    
    Added scaffold for ERC165Enhanced
    
    
    Added scaffold for ERC721Receiver abstract contract

--- contracts/datatypes/collections/EnumerableSet.sol
+++ contracts/datatypes/collections/EnumerableSet.sol
@@ -28,6 +28,103 @@ import "hardhat/console.sol";
  * (`UintSet`) are supported.
  */
 library EnumerableSet {
+
+    struct TinySet {
+        // Storage of set values
+        bytes4[] _values;
+
+        // Position of the value in the `values` array, plus 1 because index 0
+        // means a value is not in the set.
+        mapping (bytes4 => uint256) _indexes;
+    }
+
+    /**
+     * @dev Add a value to a set. O(1).
+     *
+     * Returns true if the value was added to the set, that is if it was not
+     * already present.
+     */
+    function _add(TinySet storage tinySet_, bytes32 value_) private returns (bool) {
+        if (!_contains(tinySet_, value_)) {
+            tinySet_._values.push(value_);
+            // The value is stored at length-1, but we add 1 to all indexes
+            // and use 0 as a sentinel value
+            tinySet_._indexes[value_] = tinySet_.value_.length;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @dev Removes a value from a set. O(1).
+     *
+     * Returns true if the value was removed from the set, that is if it was
+     * present.
+     */
+    function _remove(TinySet storage tinySet_, bytes32 value_) private returns (bool) {
+        // We read and store the value's index to prevent multiple reads from the same storage slot
+        uint256 valueIndex_ = tinySet_._indexes[value_];
+
+        if (valueIndex != 0) { // Equivalent to contains(set, value)
+            // To delete an element from the _values array in O(1), we swap the element to delete with the last one in
+            // the array, and then remove the last element (sometimes called as 'swap and pop').
+            // This modifies the order of the array, as noted in {at}.
+
+            uint256 toDeleteIndex_ = valueIndex_ - 1;
+            uint256 lastIndex_ = tinySet_._values.length - 1;
+
+            // When the value to delete is the last one, the swap operation is unnecessary. However, since this occurs
+            // so rarely, we still do the swap anyway to avoid the gas cost of adding an 'if' statement.
+
+            bytes32 lastvalue_ = tinySet_._values[lastIndex_];
+
+            // Move the last value to the index where the value to delete is
+            set._values[toDeleteIndex_] = lastvalue;
+            // Update the index for the moved value
+            set._indexes[lastvalue_] = toDeleteIndex_ + 1; // All indexes are 1-based
+
+            // Delete the slot where the moved value was stored
+            tinySet_._values.pop();
+
+            // Delete the index for the deleted slot
+            delete tinySet_._indexes[value_];
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @dev Returns true if the value is in the set. O(1).
+     */
+    function _contains(TinySet storage tinySet_, bytes32 value_) private view returns (bool) {
+        return tinySet_._indexes[value_] != 0;
+    }
+
+    /**
+     * @dev Returns the number of values on the set. O(1).
+     */
+    function _length(TinySet storage tinySet_) private view returns (uint256) {
+        return tinySet_._values.length;
+    }
+
+   /**
+    * @dev Returns the value stored at position `index` in the set. O(1).
+    *
+    * Note that there are no guarantees on the ordering of values inside the
+    * array, and it may change when more values are added or removed.
+    *
+    * Requirements:
+    *
+    * - `index` must be strictly less than {length}.
+    */
+    function _at(TinySet storage tinySet_, uint256 index_) private view returns (bytes32) {
+        require(tinySet_._values.length > index_, "EnumerableSet: index out of bounds");
+        return tinySet_._values[index_];
+    }
+
     // To implement this library for multiple types with as little code
     // repetition as possible, we write it in terms of a generic Set type with
     // bytes32 values.
