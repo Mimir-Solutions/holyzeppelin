04594693accd7f481188913e1cc2fa01f088783b
cyotee doge <cyotee@gmail.com>
2021-02-06 17:50:19 -0800

    Corrected EIP2612 naming.

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -331,20 +331,20 @@ abstract contract ERC20 is IERC20, ERC1820Implementer {
     //     _decimals = decimals_;
     // }
 
-    /**
-     * @dev Hook that is called before any transfer of tokens. This includes
-     * minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * will be to transferred to `to`.
-     * - when `from` is zero, `amount` tokens will be minted for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    // Present in ERC777
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
+  /**
+   * @dev Hook that is called before any transfer of tokens. This includes
+   * minting and burning.
+   *
+   * Calling conditions:
+   *
+   * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+   * will be to transferred to `to`.
+   * - when `from` is zero, `amount` tokens will be minted for `to`.
+   * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+   * - `from` and `to` are never both zero.
+   *
+   * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+   */
+  // Present in ERC777
+  function _beforeTokenTransfer( address from_, address to_, uint256 amount_ ) internal virtual { }
 }
