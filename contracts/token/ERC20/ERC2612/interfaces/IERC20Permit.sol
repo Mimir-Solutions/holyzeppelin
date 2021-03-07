aee90b24a9d72f5110f1bb4b67ac414a670bcada
cyotee doge <cyotee@gmail.com>
2021-03-07 15:23:42 -0800

    Fixed IERC20Permit.

--- contracts/token/ERC20/ERC2612/interfaces/IERC20Permit.sol
+++ contracts/token/ERC20/ERC2612/interfaces/IERC20Permit.sol
@@ -4,21 +4,20 @@ pragma solidity 0.7.5;
 import "hardhat/console.sol";
 
 interface IERC20Permit {
-  mapping(address => Counters.Counter) private _nonces;
 
-    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    function PERMIT_TYPEHASH() external view returns ( bytes32 );
+  // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+  function PERMIT_TYPEHASH() external view returns ( bytes32 );
 
-    function DOMAIN_SEPARATOR() external view returns ( bytes32 );
+  function DOMAIN_SEPARATOR() external view returns ( bytes32 );
 
-    /**
-     * @dev See {IERC2612Permit-permit}.
-     *
-     */
-    function permit( address owner, address spender, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s ) public;
+  /**
+   * @dev See {IERC2612Permit-permit}.
+   *
+   */
+  function permit( address owner, address spender, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s ) public;
 
-    /**
-     * @dev See {IERC2612Permit-nonces}.
-     */
-    function nonces(address owner) external view returns (uint256);
+  /**
+   * @dev See {IERC2612Permit-nonces}.
+   */
+  function nonces(address owner) external view returns (uint256);
 }
